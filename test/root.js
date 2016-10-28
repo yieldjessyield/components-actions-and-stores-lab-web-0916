f931c34ca2a6e079017fb9de78b4ee4c5b8437be
JJ Seymour <jjseymour@gmail.com>
2016-10-28 15:36:50 -0400

    remove comments from root.js

--- test/root.js
+++ test/root.js
@@ -4,21 +4,11 @@ import jsdom from 'jsdom';
 import path from 'path';
 import { transformFileSync } from 'babel-core';
 
-// import babel from 'babel-core'
-// const jsdom = require('jsdom');
-// const path = require('path');
-
 import fs from 'fs';
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'));
-// const exposedProperties = ['window', 'navigator', 'document'];
-//
 global.document = jsdom.jsdom(html);
 global.window = document.defaultView;
-// global.navigator = {
-//   userAgent: 'node.js'
-// };
-
 
 before(function(done) {
   const src = path.resolve(__dirname, '..', './src/index.js');
